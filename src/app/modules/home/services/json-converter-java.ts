4f90d7538fcf746c077d23d9eaa4386671bd30f2
Renan do Nascimento Sanches <renan.sanches_123@hotmail.com>
2020-12-13 21:47:53 -0300

    apostrofo ao invés de aspas

--- src/app/modules/home/services/json-converter-java.ts
+++ src/app/modules/home/services/json-converter-java.ts
@@ -23,13 +23,13 @@ export class JsonConverterJava{
     }
 
     private createClass(obj, label, indentation) {
-        let classText = "public" + " " + "class " + label + " {\n";
-        classText = classText + this.parser(obj, indentation) + "\n}";
+        let classText = 'public' + ' ' + 'class ' + label + ' {\n';
+        classText = classText + this.parser(obj, indentation) + '\n}';
         this.classesArray.push(classText);
     }
 
     private parser(obj, indent) {
-        let output = "";
+        let output = '';
         let keys = Object.keys(obj);
         let keyNames = [];
         let getterMethods = [];
@@ -45,11 +45,11 @@ export class JsonConverterJava{
                     type = 'Date'
                   }
                     output += `private ${type} ` + keys[i];
-                    output += ";\n";
-                    getterMethods.push(indent + `public ${type} get` + keyNames[i] + "() {\n" + indent + indent + "return " + keys[i] +
-                        ";\n" + indent + "}");
-                    setterMethods.push(indent + "public void set" + keyNames[i] + `( ${type} ` + keys[i] + " ) {\n" + indent + indent +
-                        "this." + keys[i] + " = " + keys[i] + ";\n" + indent + "}");
+                    output += ';\n';
+                    getterMethods.push(indent + `public ${type} get` + keyNames[i] + '() {\n' + indent + indent + 'return ' + keys[i] +
+                        ';\n' + indent + '}');
+                    setterMethods.push(indent + 'public void set' + keyNames[i] + `( ${type} ` + keys[i] + ' ) {\n' + indent + indent +
+                        'this.' + keys[i] + ' = ' + keys[i] + ';\n' + indent + '}');
                     break;
                 case 'number':
                   let typeNumber = 'Integer';
@@ -57,44 +57,44 @@ export class JsonConverterJava{
                     typeNumber = 'Double';
                   }
                     output += `private ${typeNumber} ` + keys[i];
-                    output += ";\n";
-                    getterMethods.push(indent + `public ${typeNumber} get` + keyNames[i] + "() {\n" + indent + indent + "return " + keys[i] +
-                        ";\n" + indent + "}");
-                    setterMethods.push(indent + "public void set" + keyNames[i] + `( ${typeNumber} ` + keys[i] + " ) {\n" + indent + indent +
-                        "this." + keys[i] + " = " + keys[i] + ";\n" + indent + "}");
+                    output += ';\n';
+                    getterMethods.push(indent + `public ${typeNumber} get` + keyNames[i] + '() {\n' + indent + indent + 'return ' + keys[i] +
+                        ';\n' + indent + '}');
+                    setterMethods.push(indent + 'public void set' + keyNames[i] + `( ${typeNumber} ` + keys[i] + ' ) {\n' + indent + indent +
+                        'this.' + keys[i] + ' = ' + keys[i] + ';\n' + indent + '}');
                     break;
                 case 'boolean':
                     output += 'private Boolean ' + keys[i];
-                    output += ";\n";
-                    getterMethods.push(indent + "public Boolean get" + keyNames[i] + "() {\n" + indent + indent + "return " + keys[i] +
-                        ";\n" + indent + "}");
-                    setterMethods.push(indent + "public void set" + keyNames[i] + "( Boolean " + keys[i] + " ) {\n" + indent +
-                        indent + "this." + keys[i] + " = " + keys[i] + ";\n" + indent + "}");
+                    output += ';\n';
+                    getterMethods.push(indent + 'public Boolean get' + keyNames[i] + '() {\n' + indent + indent + 'return ' + keys[i] +
+                        ';\n' + indent + '}');
+                    setterMethods.push(indent + 'public void set' + keyNames[i] + '( Boolean ' + keys[i] + ' ) {\n' + indent +
+                        indent + 'this.' + keys[i] + ' = ' + keys[i] + ';\n' + indent + '}');
                     break;
                 default:
                     if (obj[keys[i]] instanceof Array) {
-                        output += 'ArrayList<Object> ' + keys[i] + " = new ArrayList<Object>()" + ";\n";
+                        output += 'ArrayList<Object> ' + keys[i] + ' = new ArrayList<Object>()' + ';\n';
                     } else if (obj[keys[i]] == null || obj[keys[i]] == undefined) {
-                        output += 'private String ' + keys[i] + " = null";
-                        output += ";\n";
-                        getterMethods.push(indent + "public String get" + keyNames[i] + "() {\n" + indent + indent + "return " + keys[i] +
-                            ";\n" + indent + "}");
-                        setterMethods.push(indent + "public void set" + keyNames[i] + "( String " + keys[i] + " ) {\n" + indent +
-                            indent + "this." + keys[i] + " = " + keys[i] + ";\n" + indent + "}");
+                        output += 'private String ' + keys[i] + ' = null';
+                        output += ';\n';
+                        getterMethods.push(indent + 'public String get' + keyNames[i] + '() {\n' + indent + indent + 'return ' + keys[i] +
+                            ';\n' + indent + '}');
+                        setterMethods.push(indent + 'public void set' + keyNames[i] + '( String ' + keys[i] + ' ) {\n' + indent +
+                            indent + 'this.' + keys[i] + ' = ' + keys[i] + ';\n' + indent + '}');
                     } else {
-                        this.classObj[keyNames[i]] = keyNames[i] + "Object";
-                        output += keyNames[i] + " " + this.classObj[keyNames[i]] + ";\n"; // Don't change the order. CreateClass should be called at last.
-                        getterMethods.push(indent + "public " + keyNames[i] + " get" + keyNames[i] + "() {\n" + indent + indent +
-                            "return " + this.classObj[keyNames[i]] + ";\n" + indent + "}");
-                        setterMethods.push(indent + "public void set" + keyNames[i] + "( " + keyNames[i] + " " + keys[i] +
-                            "Object ) {\n" + indent + indent + "this." + this.classObj[keyNames[i]] + " = " + keys[i] + "Object" + ";\n" +
-                            indent + "}");
+                        this.classObj[keyNames[i]] = keyNames[i] + 'Object';
+                        output += keyNames[i] + ' ' + this.classObj[keyNames[i]] + ';\n'; // Don't change the order. CreateClass should be called at last.
+                        getterMethods.push(indent + 'public ' + keyNames[i] + ' get' + keyNames[i] + '() {\n' + indent + indent +
+                            'return ' + this.classObj[keyNames[i]] + ';\n' + indent + '}');
+                        setterMethods.push(indent + 'public void set' + keyNames[i] + '( ' + keyNames[i] + ' ' + keys[i] +
+                            'Object ) {\n' + indent + indent + 'this.' + this.classObj[keyNames[i]] + ' = ' + keys[i] + 'Object' + ';\n' +
+                            indent + '}');
                         this.createClass(obj[keys[i]], keyNames[i], indent);
                     }
             }
         }
-        output += "\n\n // Getter Methods \n\n" + getterMethods.join("\n\n") + "\n\n // Setter Methods \n\n" +
-            setterMethods.join("\n\n");
+        output += '\n\n // Getter Methods \n\n' + getterMethods.join('\n\n') + '\n\n // Setter Methods \n\n' +
+            setterMethods.join('\n\n');
         return output;
     }
     
